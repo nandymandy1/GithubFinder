ad3ddf54956095b1f186451747c6a0b51e6c5b47
Narendra Maurya <narendra.maurya@in.accemy.com>
2020-02-08 05:17:33 +0530

    Build Folder added

---
+++ build/precache-manifest.6f803ca73f472913175f4c01455a4f19.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f4d52deea7db654fb5d075823b476f2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7f2e6a43ea701a1317c1",
+    "url": "/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "6995485a29f643b15161",
+    "url": "/static/css/main.5a312f80.chunk.css"
+  },
+  {
+    "revision": "7f2e6a43ea701a1317c1",
+    "url": "/static/js/2.90f6873c.chunk.js"
+  },
+  {
+    "revision": "e12e80feb955000af15fb77b433237ea",
+    "url": "/static/js/2.90f6873c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6995485a29f643b15161",
+    "url": "/static/js/main.660615ef.chunk.js"
+  },
+  {
+    "revision": "19133a5d6c7be98533aa",
+    "url": "/static/js/runtime-main.107bb8ca.js"
+  }
+]);
